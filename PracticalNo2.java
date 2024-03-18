181275921ef3ffe9b2084202a7cf04a5cdd40d50
VishrantiMeshram11 <160575060+VishrantiMeshram11@users.noreply.github.com>
2024-03-18 22:54:39 +0530

    Practical No 2 committed successfully

---
+++ PracticalNo2.java
@@ -0,0 +1,29 @@
+/* Aim: Practical No. 2
+Author:Vishranti Meshram
+Version:2.0
+Date:06 Feb 2024 */
+
+import java.util.Scanner;
+
+public class PracticalNo2 
+ {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the radius of the circle: ");
+        double radius = scanner.nextDouble();
+
+        if (radius <= 0)
+         {
+            System.out.println("Please enter a non-zero positive number.");
+        } else 
+        {
+            double perimeter = 2 * Math.PI * radius;
+            double area = Math.PI * radius * radius;
+            
+            System.out.println("Perimeter of the circle: " + perimeter);
+            
+            System.out.println("Area of the circle: " + area);
+        }
+      
+    }
+}
